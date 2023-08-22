4cbb4a93e4a75293505c52e4229a24edc561e381
cliff <cliffbakalian@gmail.com>
2023-08-22 14:00:24 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CMSC330 Fall 2023
+
+### Project links
+
+### Discussion links
+
+### Other links
+
+ + [Lecture code](https://github.com/cmsc330fall23/cmsc330fall23/tree/main/discussions)
---
+++ lecture_code/git_cheatsheet.sh
@@ -0,0 +1,20 @@
+# clone a git repository
+git clone https://github.com/cmsc330fall23/cmsc330fall23.git
+
+# add a file to be tracked
+git add README.md
+
+# take a snapshot of the repository
+git commit -m "commected about what has been changed"
+
+# push your changes to github
+git push
+
+# check what has changed ot what needs too be done to push
+git status
+
+# see a log of past changes
+git log
+
+# pull changes on github to local machine
+git pull
